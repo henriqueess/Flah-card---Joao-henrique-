75597cc78401dc94d80b082f431db7a73ab6e352
henriqueess <henrisoares2007@gmail.com>
2024-11-06 10:52:27 -0300

    Add files via upload

---
+++ henrique soares 2 b/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qua a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2?',
+    'A resposta é 4'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Kryptonita é a fraqueza de qual super-herói?',
+    'Superman'
+)
